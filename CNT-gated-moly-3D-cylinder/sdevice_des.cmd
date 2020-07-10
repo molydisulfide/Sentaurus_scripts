beb1d7ed184611d2d343b5ac6e30927aa109b5ec
Jakub Jadwiszczak <jadwiszj@tcd.ie>
2020-07-10 16:27:20 -0400

    Update sdevice_des.cmd

--- CNT-gated-moly-3D-cylinder/sdevice_des.cmd
+++ CNT-gated-moly-3D-cylinder/sdevice_des.cmd
@@ -1,53 +1,15 @@
-# SDEVICE code to run the simulation. #
-
-Device JFET {
-
+#------------ dessis_des.cmd ---------------#
 Electrode{
 {name="D" voltage=0.0 Schottky Barrier = 0.33 
 eRecVelocity = 3766000 hRecVelocity = 3766000}
 {name="S" voltage=0.0}
-}
-
-File{
-Grid="@tdr@"
-Plot="@tdrdat@"
-Current="@plot@"
-*Output="@log@"
-parameter="@parameter@"
-}
-
-Physics (Material="Moly"){
-Mobility( DopingDep HighFieldSaturation Enormal )
-EffectiveIntrinsicDensity( OldSlotboom )
-Recombination( SRH(DopingDep) )
-eQuantumPotential
-Traps(
-	(eNeutral Table=(-0.1 1.2e18 -0.2 6e18 -0.3 6e18) fromCondBand
-	eXsection=1e-9 hXsection=1e-10)
-  )
-}
-
-Physics (Material="CNT"){
-Mobility( DopingDep HighFieldSaturation Enormal )
-EffectiveIntrinsicDensity( OldSlotboom )
-Recombination( SRH(DopingDep) )
-eQuantumPotential
-}
-}
-
-Device Ionic {
-
-Electrode{
-{name="ID" voltage=-0.1}
-{name="IS" voltage=0.0}
 {name="G" voltage=0.0 WorkFunction=@WK@}
 }
-
 File{
 Grid="@tdr@"
 Plot="@tdrdat@"
 Current="@plot@"
-*Output="@log@"
+Output="@log@"
 parameter="@parameter@"
 }
 
@@ -67,7 +29,6 @@ Mobility( DopingDep HighFieldSaturation Enormal )
 EffectiveIntrinsicDensity( OldSlotboom )
 Recombination( SRH(DopingDep) )
 eQuantumPotential
-}
 }
 
 
@@ -112,39 +73,19 @@ Affinity
 ConductionBand ValenceBand
 eQuantumPotential
 }
-
-System {
-
-Vsource_pset vd (Vd 0) { dc = 0.0 }
-Vsource_pset vi (Vi 0) { dc = -0.1 }
-Vsource_pset vg (Vg 0) { dc = 0.0 }
- 
-JFET jfet ( "S"=0 "D"=Vd )
-Ionic ionic ( "IS"=0 "ID"=Vi "G"=Vg )
-
-Plot "n@node@_sys_des.plt" (time() v(Vi) v(Vd) v(Vg) i(jfet,Vd) i(ionic,Vi), i(jfet, 0), i(ionic,0))
-}
-
-
 Solve {
-
-Coupled ( Iterations= 1060){ Poisson eQuantumPotential Electron Hole Contact Circuit}
-
-
+Coupled ( Iterations= 500){ Poisson eQuantumPotential }
+Coupled { Poisson eQuantumPotential Electron Hole }
 Quasistationary(
 InitialStep= 1e-5 Increment= 1.2
-MinStep= 1e-199 MaxStep= 0.95
-#Goal{Parameter= vd.dc Voltage=@Vd@ }
-
-){ Coupled { jfet.poisson jfet.electron jfet.hole jfet.contact ionic.poisson ionic.electron ionic.hole ionic.contact circuit}}
-
+MinStep= 1e-45 MaxStep= 0.95
+Goal { Name= "D" Voltage=@Vd@ }
+){ Coupled { Poisson eQuantumPotential Electron Hole } }
 Quasistationary(
 InitialStep= 1e-5 Increment= 1.2
-MinStep= 1e-199 MaxStep= 0.95
-#Goal {Parameter= vi.dc Voltage=@Vi@ }
-Goal {Parameter= vg.dc Voltage=@Vg@ }
-){ Coupled { jfet.poisson jfet.electron jfet.hole jfet.contact ionic.poisson ionic.electron ionic.hole ionic.contact circuit} }
+MinStep= 1e-45 MaxStep= 0.02
+Goal { Name= "G" Voltage=@Vg@ }
+DoZero
+){ Coupled { Poisson eQuantumPotential Electron Hole } }
 }
 #----------------------------------- END --------------------------------------#
-
-
