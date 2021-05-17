2b8e16fca9b06666e68f8e14ac903165a743ba8e
Jakub Jadwiszczak <jadwiszj@tcd.ie>
2021-05-17 12:01:52 -0400

    Create JFET_flat_des.cmd

---
+++ JFET_flat_des.cmd
@@ -0,0 +1,94 @@
+#------------ dessis_des.cmd ---------------#
+Electrode{
+{name="D" voltage=0.0}
+{name="S" voltage=0.0}
+{name="G" voltage=0.0 WorkFunction=@WK@}
+}
+File{
+Grid="@tdr@"
+Plot="@tdrdat@"
+Current="@plot@"
+Output="@log@"
+parameter="@parameter@"
+}
+
+Physics (Material="Moly"){
+
+Temperature = 300
+Mobility( DopingDep HighFieldSaturation Enormal )
+EffectiveIntrinsicDensity( OldSlotboom )
+Recombination( SRH(DopingDep) Band2Band)
+eQuantumPotential
+Traps(
+	(eNeutral Table=(-0.1 1.2e18 -0.2 6e18 -0.3 6e18) fromCondBand
+	eXsection=1e-9 hXsection=1e-10)
+  )
+}
+
+Physics (Material="CNT"){
+Temperature = 300
+Mobility( DopingDep HighFieldSaturation Enormal )
+EffectiveIntrinsicDensity( OldSlotboom )
+Recombination( SRH(DopingDep) Band2Band)
+eQuantumPotential
+}
+
+
+Math{
+-CheckUndefinedModels
+Number_Of_Threads=4
+Extrapolate
+Derivatives
+* Avalderivatives
+RelErrControl
+Digits=5
+ErRef(electron)=1.e10
+ErRef(hole)=1.e10
+Notdamped=50
+Iterations=150
+Directcurrent
+Method=ParDiSo
+Parallel= 2
+NaturalBoxMethod
+NoSRHperPotential
+}
+
+Plot{
+eDensity hDensity
+eCurrent hCurrent
+TotalCurrent/Vector eCurrent/Vector hCurrent/Vector
+eMobility hMobility
+eVelocity hVelocity
+eEnormal hEnormal
+ElectricField/Vector Potential SpaceCharge
+eQuasiFermi hQuasiFermi
+Potential Doping SpaceCharge
+SRH Auger
+AvalancheGeneration
+DonorConcentration AcceptorConcentration
+Doping
+eGradQuasiFermi/Vector hGradQuasiFermi/Vector
+eEparallel hEparalllel
+eTrappedCharge
+BandGap
+BandGapNarrowing
+Affinity
+ConductionBand ValenceBand
+eQuantumPotential
+}
+Solve {
+Coupled ( Iterations= 1800){ Poisson eQuantumPotential }
+Coupled { Poisson eQuantumPotential Electron Hole }
+Quasistationary(
+InitialStep= 1e-5 Increment= 1.2
+MinStep= 1e-275 MaxStep= 0.95
+Goal { Name= "D" Voltage=@Vd@ }
+){ Coupled { Poisson eQuantumPotential Electron Hole } }
+Quasistationary(
+InitialStep= 1e-5 Increment= 1.2
+MinStep= 1e-275 MaxStep= 0.02
+Goal { Name= "G" Voltage=@Vg@ }
+DoZero
+){ Coupled { Poisson eQuantumPotential Electron Hole } }
+}
+#----------------------------------- END --------------------------------------#
